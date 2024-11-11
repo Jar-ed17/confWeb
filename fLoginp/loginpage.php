3cc0668134c4691ebf1edd1b84bc0227b31c1004
Jar-ed17 <jared17reyes@gmail.com>
2024-11-10 21:31:37 -0500

    new login page(only views)

--- fLoginp/loginpage.php
+++ fLoginp/loginpage.php
@@ -7,23 +7,29 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container-all">
 
         <div class="ctn-form">
-            <img src="" alt="" class="logo">
+            <img src="../logo1.jpg" alt="" class="logo">
             <h1 class="title">Iniciar Sesion</h1>
 
             <form action="">
 
-                <label for="Email"></label>
+                <label for="">Email</label>
                 <input type="text">
-                <label for="Email"></label>
+                <label for="">Contraseña</label>
                 <input type="password">
-                <input type="submit" value="Iniciar">
-            </form>
-            <span class="text-footer">Aun no te haz registrado?<a href="re">Registrate</a> </span>
-        </div>
 
+                <input type="submit" value="Iniciar sesion">
+            </form>
+            <span class="text-footer">Aun no te haz registrado? <a href="registerpage">Registrate!</a> 
+            </span>
+        </div>
+        <div class="ctn-text">
+            <div class="capa"></div>
+            <h1 class="title-description">Inicia sesion en Quick Conference</h1>
+            <p class="text-description">Esto es un ejemplo ya que nose que poner en esto</p>
+        </div>
     </div>
     
 </body>
